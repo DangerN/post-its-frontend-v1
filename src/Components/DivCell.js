55649a67d46f97198bd1b6ff3a086342e587a447
DangerN <cdneal091@gmail.com>
2019-05-28 22:48:04 -0600

    muh grids

---
+++ src/Components/DivCell.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const DivCell = props => {
+    return (
+      <div style={props.style}/>
+    )
+  }
+export default DivCell
--- src/Components/NoteBoard.js
+++ src/Components/NoteBoard.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react'
+import DivCell from './DivCell'
 
 export default class NoteBoard extends Component {
+  createPinHooks() {
+    let divBucket = []
+    for(let i=0; i < 40; i++) {
+      for(let j=0; j < 40; j++) {
+        const props = {
+          style: {
+            gridColumnStart: i,
+            gridRowStart: j
+          },
+          keyProp: `${i}${j}${j}`
+        }
+        divBucket.push(<DivCell key={props.keyProp} {...props} />)
+      }
+    }
+    return (
+      divBucket
+    )
+  }
   render() {
     return(
       <div id='note-board'>
-        i am the NoteBoard hear me roar
+        {this.createPinHooks()}
       </div>
     )
   }
